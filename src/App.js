3cb69d97945e4eb86eb78f78de5a24444e7769b6
mallouk2020 <elyassmallouk@gmail.com>
2025-01-26 00:09:10 +0100

    secend-commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+// import Form from './form';
+import Prayercard from './components/pryercard';
+
+function App() {
+  return (
+<div className='allapp'>
+    <div className='App' style={{display:"flex", justifyContent: "center", flexDirection:"column", width:"90vw"}}>
+
+
+
+<Prayercard/>
+
+
+
+      </div>
+      </div>
+
+      );
+}
+
+      export default App;
